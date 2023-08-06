b48ce15bcfd3b74879d7f6f0c38ea071b535e16a
Romalvo <roman.voronkov.06@gmail.com>
2023-08-06 12:58:41 +0300

    Add new test case for create new issue with checking spaces

--- cypress/e2e/issue-create.cy.js
+++ cypress/e2e/issue-create.cy.js
@@ -6,15 +6,15 @@ describe('Issue create', () => {
   beforeEach(() => {
     cy.visit('/');
     cy.url().should('eq', 'https://jira.ivorreic.com/project').then((url) => {
-    //System will already open issue creating modal in beforeEach block  
-    cy.visit(url + '/board?modal-issue-create=true');
+      //System will already open issue creating modal in beforeEach block  
+      cy.visit(url + '/board?modal-issue-create=true');
     });
   });
 
   it('Should create an issue and validate it successfully', () => {
     //System finds modal for creating issue and does next steps inside of it
     cy.get('[data-testid="modal:issue-create"]').within(() => {
-      
+
       //open issue type dropdown and choose Story
       cy.get('[data-testid="select:type"]').click();
       cy.get('[data-testid="select-option:Story"]').click();
@@ -22,8 +22,8 @@ describe('Issue create', () => {
       cy.get('[data-testid="select:type"]').click();
       cy.get('[data-testid="icon:close"]').click();
       cy.get('[data-testid="select-option:Story"]')
-          .trigger('click');
-            
+        .trigger('click');
+
       //Type value to description input field
       cy.get('.ql-editor').type('TEST_DESCRIPTION');
 
@@ -31,7 +31,7 @@ describe('Issue create', () => {
       //Order of filling in the fields is first description, then title on purpose
       //Otherwise filling title first sometimes doesn't work due to web page implementation
       cy.get('input[name="title"]').type('TEST_TITLE');
-      
+
       //Select Lord Gaben from (reporter)? dropdown
       cy.get('[data-testid="select:userIds"]').click();
       cy.get('[data-testid="select-option:Lord Gaben"]').click();
@@ -43,7 +43,7 @@ describe('Issue create', () => {
     //Assert that modal window is closed and successful message is visible
     cy.get('[data-testid="modal:issue-create"]').should('not.exist');
     cy.contains('Issue has been successfully created.').should('be.visible');
-    
+
     //Reload the page to be able to see recently created issue
     //Assert that successful message has dissappeared after the reload
     cy.reload();
@@ -53,18 +53,18 @@ describe('Issue create', () => {
     cy.get('[data-testid="board-list:backlog').should('be.visible').and('have.length', '1').within(() => {
       //Assert that this list contains 5 issues and first element with tag p has specified text
       cy.get('[data-testid="list-issue"]')
-          .should('have.length', '5')
-          .first()
-          .find('p')
-          .contains('TEST_TITLE');
+        .should('have.length', '5')
+        .first()
+        .find('p')
+        .contains('TEST_TITLE');
       //Assert that correct avatar and type icon are visible
       //in this case it will be checked from all list of issues where located avatar or icon what we need!!!
       //cy.get('[data-testid="avatar:Lord Gaben"]').should('be.visible');
       //cy.get('[data-testid="icon:story"]').should('be.visible');
       cy.get('[data-testid="list-issue"]')
-          .first().find('[data-testid="avatar:Lord Gaben"]').should('be.visible');
+        .first().find('[data-testid="avatar:Lord Gaben"]').should('be.visible');
       cy.get('[data-testid="list-issue"]')
-          .first().find('[data-testid="icon:story"]').should('be.visible');
+        .first().find('[data-testid="icon:story"]').should('be.visible');
     });
   });
 
@@ -82,14 +82,14 @@ describe('Issue create', () => {
   it('Should create an issue with special data and validate it successfully', () => {
     //System finds modal for creating issue and does next steps inside of it
     cy.get('[data-testid="modal:issue-create"]').within(() => {
-      
+
       cy.get('[data-testid="select:type"]').click();
       cy.get('[data-testid="select-option:Bug"]').click();
       //slow down test to get correct choice
       cy.get('[data-testid="select:type"]').click();
       cy.get('[data-testid="icon:close"]').click();
       cy.get('[data-testid="select-option:Bug"]')
-          .trigger('click');  
+        .trigger('click');
       cy.get('.ql-editor').type('My bug description');
       cy.get('input[name="title"]').type('Bug');
       cy.get('[data-testid="select:reporterId"]').click();
@@ -102,19 +102,19 @@ describe('Issue create', () => {
     //Assert that modal window is closed and successful message is visible
     cy.get('[data-testid="modal:issue-create"]').should('not.exist');
     cy.contains('Issue has been successfully created.').should('be.visible');
-    
+
     cy.reload();
     cy.contains('Issue has been successfully created.').should('not.exist');
 
     //Assert than only one list with name Backlog is visible and do steps inside of it
     cy.get('[data-testid="board-list:backlog').should('be.visible').and('have.length', '1').within(() => {
       cy.get('[data-testid="list-issue"]')
-          .should('have.length', '5')
-          .first()
-          .find('p')
-          .contains('Bug');
+        .should('have.length', '5')
+        .first()
+        .find('p')
+        .contains('Bug');
       cy.get('[data-testid="list-issue"]')
-          .first().find('[data-testid="icon:bug"]').should('be.visible');
+        .first().find('[data-testid="icon:bug"]').should('be.visible');
     });
   });
 
@@ -122,7 +122,7 @@ describe('Issue create', () => {
   it('Should create an issue with random data in title and description and validate it successfully', () => {
     //System finds modal for creating issue and does next steps inside of it
     cy.get('[data-testid="modal:issue-create"]').within(() => {
-      
+
       cy.get('[data-testid="select:type"]').click();
       cy.get('[data-testid="icon:close"]').click();
       cy.get('[data-testid="select-option:Task"]').click();
@@ -130,7 +130,7 @@ describe('Issue create', () => {
       cy.get('[data-testid="select:type"]').click();
       cy.get('[data-testid="icon:close"]').click();
       cy.get('[data-testid="select-option:Task"]')
-          .trigger('click'); 
+        .trigger('click');
       cy.get('.ql-editor').type(randomDescription);
       cy.get('input[name="title"]').type(randomTitle);
       cy.get('[data-testid="select:reporterId"]').click();
@@ -143,20 +143,58 @@ describe('Issue create', () => {
     //Assert that modal window is closed and successful message is visible
     cy.get('[data-testid="modal:issue-create"]').should('not.exist');
     cy.contains('Issue has been successfully created.').should('be.visible');
-    
+
     cy.reload();
     cy.contains('Issue has been successfully created.').should('not.exist');
 
     //Assert than only one list with name Backlog is visible and do steps inside of it
     cy.get('[data-testid="board-list:backlog"]').should('be.visible').and('have.length', '1').within(() => {
       cy.get('[data-testid="list-issue"]')
-          .should('have.length', '5')
-          .first()
-          .find('p')
-          .contains(randomTitle);
+        .should('have.length', '5')
+        .first()
+        .find('p')
+        .contains(randomTitle);
       //Assert that correct type icon are visible
-    cy.get('[data-testid="list-issue"]')
-         .first().find('[data-testid="icon:task"]').should('be.visible');
+      cy.get('[data-testid="list-issue"]')
+        .first().find('[data-testid="icon:task"]').should('be.visible');
+    });
+  });
+  it.only('Should create an issue with multiple spaces betwwen words in title and validate it successfully', () => {
+    const title = ' Hello world ';
+    const trimmedTitle = title.trim();
+    cy.get('[data-testid="modal:issue-create"]').within(() => {
+
+      cy.get('[data-testid="select:type"]').click();
+      cy.get('[data-testid="icon:close"]').click();
+      cy.get('[data-testid="select-option:Bug"]').click();
+      //slow down test to get correct choice    
+      cy.get('[data-testid="select:type"]').click();
+      cy.get('[data-testid="icon:close"]').click();
+      cy.get('[data-testid="select-option:Bug"]')
+        .trigger('click');
+      cy.get('.ql-editor').type(randomDescription);
+      cy.get('input[name="title"]').type(title);
+      cy.get('[data-testid="select:reporterId"]').click();
+      cy.get('[data-testid="select-option:Baby Yoda"]').click();
+      cy.get('[data-testid="select:priority"]').click();
+      cy.get('[data-testid="select-option:Low"]').click();
+      cy.get('button[type="submit"]').click();
+    });
+    //Assert that modal window is closed and successful message is visible
+    cy.get('[data-testid="modal:issue-create"]').should('not.exist');
+    cy.contains('Issue has been successfully created.').should('be.visible');
+    cy.reload();
+    cy.contains('Issue has been successfully created.').should('not.exist');
+    //Assert than only one list with name Backlog is visible and do steps inside of it
+    cy.get('[data-testid="board-list:backlog"]').should('be.visible').and('have.length', '1').within(() => {
+      cy.get('[data-testid="list-issue"]')
+        .should('have.length', '5')
+        .first()
+        .find('p')
+        .contains(trimmedTitle);
+      //Assert that correct type icon are visible
+      cy.get('[data-testid="list-issue"]')
+        .first().find('[data-testid="icon:bug"]').should('be.visible');
     });
   });
 });
